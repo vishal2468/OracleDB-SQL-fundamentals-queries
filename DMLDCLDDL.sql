31bc287036894e05f1d86be08db8f422caef0e04
vishal2468 <vishalpoddar2468@gmail.com>
2022-12-16 20:14:15 +0530

    dmlddldcl

---
+++ DMLDCLDDL.sql
@@ -0,0 +1,29 @@
+-- Data manipulation language (DML)
+    -- SELECT
+    -- INSERT
+    -- UPDATE
+    -- DELETE
+    -- MERGE
+
+-- Data definition language (DDL)
+    -- CREATE
+    -- ALTER
+    -- DROP
+    -- RENAME
+    -- TRUNCATE
+    -- COMMENT
+
+-- Data control language (DCL)
+    -- GRANT 
+    -- REVOKE
+
+-- Transaction control 
+    -- SAVEPOINT
+    -- COMMIT
+    -- ROLLBACK
+
+-- DDL (Data Definition Language) commands are used to define the database structure or schema.
+-- DQL (Data Query Language) commands are used to retrieve data from the database. 
+-- DML (Data Modification Language) commands are used to modify data in the database. 
+-- DCL (Data Control Language) commands are used to control access to the database. 
+-- TCL (Transaction Control Language) commands are used to manage the transactions in the database. 
